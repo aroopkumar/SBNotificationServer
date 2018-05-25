71af4af4506a7d43fb1875f4240ac9e881216cb8
root <root@localhost.localdomain>
2018-05-25 11:09:54 +0530

    Initial commit

---
+++ src/main/java/com/ttn/notificationsb/NotificationsbApplication.java
@@ -0,0 +1,12 @@
+package com.ttn.notificationsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationsbApplication.class, args);
+	}
+}
